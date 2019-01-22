89f52ed730ec40fe05e6180e8bd6d3b2b2acb86b
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-01-22 15:42:20 -0500

    Created class and everything is set up, trying to get multiplication to work

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Calculator {
+
+    private int height;
+    private int width;
+    private int area;
+
+    public void setHeight (int objectHeight) {
+        height = objectHeight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth (int objectWidth) {
+        width = objectWidth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setArea (int objectArea) {
+        area = objectArea;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+
+}
---
+++ src/com/company/CalculatorDemo.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class CalculatorDemo {
+
+    public static void main(String[] args) {
+
+        Scanner key = new Scanner(System.in);
+        Calculator firstCalculation = new Calculator();
+        int inputHeight;
+        int inputWidth;
+
+        System.out.println("This is a calculator that will find the area of ANY rectangle you come across");
+        System.out.println("Please enter your height: ");
+
+        inputHeight = key.nextInt();
+
+        System.out.println("Please enter your width: ");
+
+        inputWidth = key.nextInt();
+
+        firstCalculation.setHeight(inputHeight);
+        firstCalculation.setWidth(inputWidth);
+
+        System.out.println("The area of said rectangle is: " + firstCalculation.getHeight() + " * " + firstCalculation.getWidth() + " = " + firstCalculation.getArea());
+
+
+
+    }
+}
